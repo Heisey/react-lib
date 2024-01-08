03c9100225992cdc020b73543a5b2efaf4c4fdd9
Heisey <jheisler01@gmail.com>
2024-01-08 12:03:32 -0800

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react'
+
+const App: React.FC = () => {
+
+  return (
+    <div>
+      puppy app
+    </div>
+  )
+}
+
+export const Component = App
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import * as App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App.Component />
+  </React.StrictMode>,
+)
