2b27b756df450a27bbaac070dd13ae1f7131593f
Heisey <jheisler01@gmail.com>
2024-01-18 03:53:56 -0800

    updated core exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,13 +1,13 @@
 
 
-import * as _config from './config'
+import * as config from './config'
 
-import * as _keys from './keys'
+import * as keys from './keys'
 
 import type * as I from './interfaces'
 
-export const config = _config
-
-export const keys = _keys
-
-export { I }
+export { 
+  I,
+  config,
+  keys 
+}
