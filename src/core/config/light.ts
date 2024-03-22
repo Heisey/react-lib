c498a47353243315485136aab678f93133b5877f
Heisey <jheisler01@gmail.com>
2024-03-22 11:20:14 -0700

    updated light theme

--- src/core/config/light.ts
+++ src/core/config/light.ts
@@ -1,2 +1,46 @@
 
-export const light = {}
+import * as Core_I from '../interfaces'
+import * as Core_Keys from '../keys'
+
+export const light: Core_I.Theme = {
+  name: 'dark',
+
+  app: {
+    maxWidth: '1920px',
+    backgroundColor: 'black'
+  },
+  button: {
+    [Core_Keys.buttonThemes.PRIMARY]: {
+      fg: 'white',
+      bg: 'purple'
+    },
+    [Core_Keys.buttonThemes.SECONDARY]: {
+      fg: 'white',
+      bg: 'darkblue'
+    },
+    [Core_Keys.buttonThemes.WARNING]: {
+      fg: 'black',
+      bg: 'yellow'
+    },
+    [Core_Keys.buttonThemes.DANGER]: {
+      fg: 'black',
+      bg: 'red'
+    },
+    [Core_Keys.buttonThemes.INFO]: {
+      fg: 'white',
+      bg: 'grey'
+    },
+    radius: '15px',
+    boxShadow: 'none',
+    paddingX: '24px',
+    paddingY: '8px'
+  },
+
+  typography: {
+    [Core_Keys.typographyThemes.DARK]: 'black',
+    [Core_Keys.typographyThemes.LIGHT]: 'white',
+    [Core_Keys.typographyThemes.INFO]: 'lightgrey',
+    [Core_Keys.typographyThemes.WARNING]: 'yellow',
+    [Core_Keys.typographyThemes.DANGER]: 'red'
+  }
+}
