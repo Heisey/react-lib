f674c2c4e9692d417a5d3643da189f01bf43bbc5
Heisey <jheisler01@gmail.com>
2024-03-24 21:35:23 -0700

    added more styles to buttons

--- src/components/Buttons/Button/styles.ts
+++ src/components/Buttons/Button/styles.ts
@@ -18,9 +18,19 @@ const border = (theme: Core.I.Theme, args: Props) => {
   return `2px solid ${theme.button[args.buttontheme].bg}`
 } 
 
+const boxShadow = (theme: Core.I.Theme, args: Props) => {
+  if (args.variant === Core.keys.buttonVaraints.TEXT) return 'none'
+  return theme.button.boxShadow
+}
+
 const color = (theme: Core.I.Theme, args: Props) => {
   if (args.variant !== Core.keys.buttonVaraints.SOLID) return theme.button[args.buttontheme].bg
   return theme.button[args.buttontheme].fg
+}
+
+const padding = (theme: Core.I.Theme, args: Props) => {
+  if (args.variant === Core.keys.buttonVaraints.TEXT) return 0
+  return `${theme.button.paddingY} ${theme.button.paddingX}`
 }
 
 export const Button = Styled.button<Props>`
@@ -28,8 +38,8 @@ export const Button = Styled.button<Props>`
   color: ${args => color(args.theme, args)};
   border: ${args => border(args.theme, args)};
   border-radius: ${args => args.theme.button.radius};
-  box-shadow: ${args => args.theme.button.boxShadow};
-  padding: ${args => `${args.theme.button.paddingY} ${args.theme.button.paddingX}`};
+  box-shadow: ${args => boxShadow(args.theme, args)};
+  padding: ${args => padding(args.theme, args)};
   outline: none;
   cursor: pointer;
 `
