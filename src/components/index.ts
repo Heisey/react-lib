53a553f10d46cc0312a98f057a798789b83f1539
Heisey <jheisler01@gmail.com>
2024-03-24 20:11:33 -0700

    added layout to exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,15 +3,22 @@ import * as _Buttons from './Buttons'
 
 import * as _Images from './Images'
 
+import * as _Layout from './Layout'
+
 import * as _Navigation from './Navigation'
 
 import * as _Providers from './Providers'
 
 import * as _Typography from './Typography'
 
+
+
+
 export const Buttons = _Buttons
 
 export const Images = _Images
+
+export const Layout = _Layout
 
 export const Navigation = _Navigation
 
