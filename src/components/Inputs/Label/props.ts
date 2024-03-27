e4cd923630c26bbbdb68278267ba4e102853d33f
Heisey <jheisler01@gmail.com>
2024-03-26 21:39:59 -0700

    updated label props

--- src/components/Inputs/Label/props.ts
+++ src/components/Inputs/Label/props.ts
@@ -1,7 +1,7 @@
 
 import * as React from 'react'
 
-export interface LabelProps extends React.HTMLAttributes<HTMLLabelElement> {
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
   required?: boolean
   input: React.ReactNode
   field?: string
