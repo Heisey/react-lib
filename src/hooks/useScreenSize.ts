aff1efb68c7b41a206c826174561c561707e6dc4
Heisey <jheisler01@gmail.com>
2024-04-04 12:07:03 -0700

    added hook

---
+++ src/hooks/useScreenSize.ts
@@ -0,0 +1,27 @@
+
+import * as React from 'react';
+
+export const useScreenSize = () => {
+  const [screenSize, setScreenSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
